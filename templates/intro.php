c99892e0022e765a46937e57871c5bbbbff92c62
Jesus <jesus@esbrillante.mx>
2018-04-02 17:11:13 -0500

    Diseño de pagina de idioma

--- templates/intro.php
+++ templates/intro.php
@@ -1,14 +1,17 @@
 <?php
     /* Template Name: Intro */ 
-    get_headers();
+    get_header();
 ?>
 
-<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg" alt="MCL" class="logo">
-<div class="flex">
-    <div class="columna"><a href="#">ESPAÑOL</a></div>
-    <div class="columna"><a href="#">ENGLISH</a></div>
-</div>
+    <section id="idioma">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg" alt="MCL" class="logo">
+        <div class="flex center-h nowrap idiomas">
+            <div class="columna"><a href="#">ESPAÑOL</a></div>
+            <div class="columna"><a href="#">ENGLISH</a></div>
+        </div>
 
-<?php 
+    </section>
+
+    <?php 
     get_footer();
 ?>
