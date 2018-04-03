a25cf270ea2355fdff5d201fe57430b6bd460aa5
Jesus <jesus@esbrillante.mx>
2018-04-02 20:43:58 -0500

    Seccion de intro e Inicio

---
+++ footer.php
@@ -0,0 +1,96 @@
+<?php
+global $mk_options;
+
+$mk_footer_class = $show_footer = $disable_mobile = $footer_status = '';
+
+$post_id = global_get_post_id();
+if ( $post_id ) {
+	$show_footer = get_post_meta( $post_id, '_template', true );
+	$cases = array( 'no-footer', 'no-header-footer', 'no-header-title-footer', 'no-footer-title' );
+	$footer_status = in_array( $show_footer, $cases );
+}
+
+if ( $mk_options['disable_footer'] == 'false' || ( $footer_status ) ) {
+	$mk_footer_class .= ' mk-footer-disable';
+}
+
+if ( $mk_options['footer_type'] == '2' ) {
+	$mk_footer_class .= ' mk-footer-unfold';
+}
+
+
+$boxed_footer = (isset( $mk_options['boxed_footer'] ) && ! empty( $mk_options['boxed_footer'] )) ? $mk_options['boxed_footer'] : 'true';
+$footer_grid_status = ($boxed_footer == 'true') ? ' mk-grid' : ' fullwidth-footer';
+$disable_mobile = ($mk_options['footer_disable_mobile'] == 'true' ) ? $mk_footer_class .= ' disable-on-mobile' : ' ';
+
+?>
+
+
+    <footer id="footer" class="template-section padding-50">
+        <div class="contenedor">
+            <div class="redes flex">
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons/linkedin.png" alt="Linkedin"></a>
+            </div>
+
+            <div class="enlaces flex">
+                <a href="/contacto">Contáctenos</a>
+                <a href="/blog">Blog</a>
+                <a href="/aviso-de-privacidad">Aviso de Privacidad</a>
+            </div>
+
+            <div class="copyright">
+                <p>© Magna Carta Lex 2018. <br> Todos los derechos reservados.
+                </p>
+            </div>
+            
+            <a href="https://esbrillante.mx/diseno-de-paginas-web-profesionales-empresas/" title="Diseño profesional de Sitios Web" class="esbrillante">Sitio Web diseñado por Esbrillante</a>
+        </div>
+    </footer>
+
+
+    </div>
+    <?php
+	global $is_header_shortcode_added;
+
+	/**
+	 * After new changed, it will return null if there is no header shortcode
+	 * added. Need to check and save it as array if it's null to avoid error.
+	 *
+	 * @since 6.0.3
+	 * @see /components/shortcodes/mk_header/config.php
+	 */
+	if ( ! is_array( $is_header_shortcode_added ) ) {
+		$is_header_shortcode_added = array();
+	}
+
+if ( $mk_options['seondary_header_for_all'] === 'true' || get_header_style() === '3' || in_array( '3', $is_header_shortcode_added, true ) ) {
+	mk_get_header_view(
+		'holders', 'secondary-menu', [
+			'header_shortcode_style' => $is_header_shortcode_added,
+		]
+	);
+}
+?>
+        </div>
+
+        <?php
+if ( $mk_options['header_search_location'] === 'fullscreen_search' ) {
+	mk_get_header_view( 'global', 'full-screen-search' );
+}
+?>
+
+            <?php if ( ! empty( $mk_options['body_border'] ) && $mk_options['body_border'] === 'true' ) { ?>
+            <div class="border-body border-body--top"></div>
+            <div class="border-body border-body--left border-body--side"></div>
+            <div class="border-body border-body--right border-body--side"></div>
+            <div class="border-body border-body--bottom"></div>
+            <?php } ?>
+
+            <?php wp_footer(); ?>
+
+            <?php do_action( 'mk_theme_before_body_closing' ); ?>
+            </body>
+
+            </html>
--- templates/home.php
+++ templates/home.php
@@ -1,67 +1,142 @@
 <?php
     /* Template Name: Principal */ 
-    get_headers();
+    get_header();
 ?>
 
-<section id="amparo">
-    <div class="flex">
-        <div class="columna">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/librosApilados.png" alt="Libros apilados">
+    <section id="amparo" class="template-section">
+        <div class="contenedor">
+            <div class="flex">
+                <div class="columna">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/amparo.png" alt="Servicio de amparo">
+                </div>
+                <div class="columna align-self-center">
+                    <h2>¿Necesita un amparo?</h2>
+                    <p>Todo acto de autoridad violatorio de derechos humanos debe ser restituido o debe la autoridad cumplir con lo que el derecho exija.
+                    </p>
+
+                    <a href="/contacto" class="boton azul">Contáctenos</a>
+                </div>
+            </div>
         </div>
-        <div class="columna">
-            <h2>¿Necesita un amparo?</h2>
-            <p>Todo acto de autoridad violatorio de derechos humanos debe ser restituido o debe la autoridad cumplir con lo que el derecho exija. 
-            </p>
-            <button>Contáctenos</button>
+
+    </section>
+
+    <section id="mcl" class="template-section">
+
+        <div class="flex">
+            <div class="columna">
+                <article class="quienes-somos">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/MCL.png" alt="MCL">
+
+                    <h2>Magna Carta Lex</h2>
+
+                    <p class="justify">Somos una firma de abogadas y abogados especializada en amparo, así como en derechos humanos y en temas constitucionales y referentes a las soberanías de los Estados, en México y/o en los diversos mecanismos internacionales, como por ejemplo la Corte Internacional de Justicia, la Comisión Interamericana de Derechos Humanos o la Corte Interamericana de Derechos Humanos, que trata a sus clientas y clientes de manera personalizada, en un entorno de familiaridad.</p>
+
+                    <p class="justify">Nuestros productos y servicios jurídicos pasan por un riguroso proceso de perfeccionamiento que tiene como destino la excelencia.</p>
+                </article>
+
+
+
+            </div>
+            <div class="columna"></div>
         </div>
-    </div>
-</section>
 
-<section id="mcl">
-    <div class="flex">
-        <div class="columna">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/MCL.png" alt="MCL">
-            <h1>Magna Carta Lex</h1>
-            <p class="sup">Somos una firma de abogadas y abogados especializada en amparo, así como en derechos humanos y en temas constitucionales y referentes a las soberanías de los Estados, en México y/o en los diversos mecanismos internacionales, como por ejemplo la Corte Internacional de Justicia, la Comisión Interamericana de Derechos Humanos o la Corte Interamericana de Derechos Humanos, que trata a sus clientas y clientes de manera personalizada, en un entorno de familiaridad.</p>
-            <p class="inf">Nuestros productos y servicios jurídicos pasan por un riguroso proceso de perfeccionamiento que tiene como destino la excelencia.</p>
+
+    </section>
+
+    <section id="pro" class="template-section padding-100">
+        <h2 class="text-center">
+            <i>Pro bono y</i>equidad de género
+        </h2>
+        <p class="sup">Creemos en la constante necesidad de impulsar el estado de las cosas hacia adelante, y en la equidad de género, que, en lo que nos compete, desemboca en la autonomía de las personas respecto a la plena defensa de sus derechos y en el trato equitativo a quienes forman parte de nuestra firma.</p>
+        <p class="inf">Trabajamos bajo el principio referente a que la mejor forma de ayudarnos, es ayudando a los demás. Por eso, cuando tomamos un caso pro bono (voluntariamente y sin retribución monetaria), más que defender a una persona en particular, pretendemos impactar positivamente a todas las personas en general. </p>
+
+        <img class="center" src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/equidadGenero.png" alt="balanza con hombre y mujer frente a la ciudad">
+    </section>
+
+    <section id="blog" class="template-section padding-100">
+        <h2 class="text-center">Blog</h2>
+        <div class="flex">
+            <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/law.png" alt="libro de leyes y martillo de juez"></div>
+            <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/librosFeosApilados.png" alt="libros apilados"></div>
+            <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/martilloColor.png" alt="martillo de juez"></div>
         </div>
-        <div class="columna">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/rey.png" alt="Rey bicolor ajedrez">
+    </section>
+
+    <section id="suscribir" class="template-section padding-100">
+        <div class="flex">
+            <div class="columna"></div>
+            <div class="columna">
+                <h2 class="text-center">Suscríbase a nuestro blog</h2>
+                <!-- Begin MailChimp Signup Form -->
+
+                <div id="mc_embed_signup">
+                    <form action="https://magnacartalex.us12.list-manage.com/subscribe/post?u=e942e1e67e8f86d86b4fddbf5&amp;id=d8127974f7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+
+                            <div class="mc-field-group">
+                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Instroduzca su correo electrónico">
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e942e1e67e8f86d86b4fddbf5_d8127974f7" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribirme" name="subscribe" id="mc-embedded-subscribe" class="boton azul"></div>
+                        </div>
+                    </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+                <script type='text/javascript'>
+                    (function($) {
+                        window.fnames = new Array();
+                        window.ftypes = new Array();
+                        fnames[0] = 'EMAIL';
+                        ftypes[0] = 'email';
+                        fnames[1] = 'FNAME';
+                        ftypes[1] = 'text';
+                        fnames[2] = 'LNAME';
+                        ftypes[2] = 'text';
+                        fnames[3] = 'ADDRESS';
+                        ftypes[3] = 'address';
+                        fnames[4] = 'PHONE';
+                        ftypes[4] = 'phone';
+                        fnames[5] = 'BIRTHDAY';
+                        ftypes[5] = 'birthday';
+                        /*
+                         * Translated default messages for the $ validation plugin.
+                         * Locale: ES
+                         */
+                        $.extend($.validator.messages, {
+                            required: "Este campo es obligatorio.",
+                            remote: "Por favor, rellena este campo.",
+                            email: "Por favor, escribe una dirección de correo válida",
+                            url: "Por favor, escribe una URL válida.",
+                            date: "Por favor, escribe una fecha válida.",
+                            dateISO: "Por favor, escribe una fecha (ISO) válida.",
+                            number: "Por favor, escribe un número entero válido.",
+                            digits: "Por favor, escribe sólo dígitos.",
+                            creditcard: "Por favor, escribe un número de tarjeta válido.",
+                            equalTo: "Por favor, escribe el mismo valor de nuevo.",
+                            accept: "Por favor, escribe un valor con una extensión aceptada.",
+                            maxlength: $.validator.format("Por favor, no escribas más de {0} caracteres."),
+                            minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
+                            rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+                            range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+                            max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+                            min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}.")
+                        });
+                    }(jQuery));
+                    var $mcj = jQuery.noConflict(true);
+
+                </script>
+                <!--End mc_embed_signup-->
+            </div>
         </div>
-    </div>
-</section>
-
-<section id="pro">
-    <h2>
-        <i>Pro bono</i>
-        y equidad de género
-    </h2>
-    <p class="sup">Creemos en la constante necesidad de impulsar el estado de las cosas hacia adelante, y en la equidad de género, que, en lo que nos compete, desemboca en la autonomía de las personas respecto a la plena defensa de sus derechos y en el trato equitativo a quienes forman parte de nuestra firma.</p>
-    <p class="inf">/</p>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/equidadGenero.png" alt="balanza con hombre y mujer frente a la ciudad">
-</section>
-
-<section id="blog">
-    <h2>Blog</h2>
-    <div class="flex">
-        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/law.png" alt="libro de leyes y martillo de juez"></div>
-        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/librosFeosApilados.png" alt="libros apilados"></div>
-        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/martilloColor.png" alt="martillo de juez"></div>
-    </div>
-</section>
-
-<section id="suscribir">
-    <div class="flex">
-        <div class="columna"></div>
-        <div class="columna">
-            <h2>Suscríbase a nuestro blog</h2>
-            <input type="text" placeholder="Introduzca su correo electrónico">
-            <button>Suscribirme</button>
-        </div>
-    </div>
-</section>
+    </section>
 
 
-<?php 
+    <?php 
     get_footer();
 ?>
